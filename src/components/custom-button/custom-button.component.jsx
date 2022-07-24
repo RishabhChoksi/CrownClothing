64286292dc2aeac342c03649cd7d9d80c038c35a
Rishabh Choksi <rishabhchoksi98@gmail.com>
2022-07-24 17:48:55 +0530

    Addition of sign-in section

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,13 @@
+import "./custom-button.styles.css";
+
+import React from "react";
+
+function CustomButton({ children, ...otherProps }) {
+  return (
+    <button className="custom-button" {...otherProps}>
+      {children}
+    </button>
+  );
+}
+
+export default CustomButton;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,55 @@
+import "./sign-in.styles.css";
+
+import React, { Component } from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+export class SignIn extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form action="" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            required
+            label={"email"}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label={"password"}
+            required
+          />
+          <CustomButton type="submit">Sign in</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
