80bcb5af6502c9219aa495c3efac5444a6db19d5
Rishabh Choksi <rishabhchoksi98@gmail.com>
2022-07-24 13:02:01 +0530

    Added Menu Item navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import { Link, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
+const Hats = (props) => {
+  console.log(props);
+  return <h1>Hats page</h1>;
+};
 
 function App() {
-  return <div><HomePage/></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<Hats />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
